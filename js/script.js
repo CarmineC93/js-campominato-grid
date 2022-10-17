922ba4b7f56ed6dd119f8d67fd9590d3af3fff3b
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-10-17 15:04:16 +0200

    Interaction: texual code sed

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
+ed emetto un messaggio in console con il numero della cella cliccata. */
+
+//1 All'evento click sul btn Play verrà generata una griglia di gioco
+    //1.1 dovrà essere generato un array di 100 numeri progressivi da 1 a 100
+    //1.2 per ogni numero dell'array creiamo una cella della griglia
+
+//2 ad ogni click su di una cella, questa cambierà colore e loggherà un messaggio con il proprio numero
+
+
+
+
